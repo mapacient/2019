cea6a7ff6768fa12e487fcc001dc6f67005a110d
mapacient <mapacient@icloud.com>
2019-05-25 21:01:48 +0800

    901

--- uuid/uuid_test.go
+++ uuid/uuid_test.go
@@ -7,7 +7,6 @@ import (
 var test_count = 7
 
 func Test_uuid16(t *testing.T) {
-	t.Log("test")
 	for i := 0; i < test_count; i++ {
 		s := Uuid16()
 		t.Log(s)
@@ -15,7 +14,6 @@ func Test_uuid16(t *testing.T) {
 }
 
 func Test_uuid(t *testing.T) {
-	t.Log("test")
 	for i := 0; i < test_count; i++ {
 		s := Uuid(3)
 		t.Log(s)
