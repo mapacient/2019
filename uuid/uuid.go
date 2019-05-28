876a3c54208bf9dedceef9250ea2a915c7c749f0
mapacient <mapacient@icloud.com>
2019-05-28 20:07:46 +0800

    lazy atomic

--- uuid/uuid.go
+++ uuid/uuid.go
@@ -4,18 +4,27 @@ import (
 	"errors"
 	"fmt"
 	"math/rand"
+	"sync/atomic"
 	"time"
 )
 
 var r *rand.Rand
 
-func init() {
-	r_s := rand.NewSource(time.Now().UnixNano())
-	r = rand.New(r_s)
+func get_rand() {
+	var v atomic.Value = atomic.Value{}
+	if v.Load() == nil {
+		//		println("v load nil")
+		r_s := rand.NewSource(time.Now().UnixNano())
+		r = rand.New(r_s)
+
+		v.Store(r)
+	}
 }
 
-//Uuid16 0x 16 0-F
 func Uuid16() string {
+	if r == nil {
+		get_rand()
+	}
 	sl := make([]byte, 8)
 	_, err := r.Read(sl)
 	if err != nil {
@@ -27,6 +36,9 @@ func Uuid16() string {
 
 //Uuid 0x l 0-F
 func Uuid(l int) string {
+	if r == nil {
+		get_rand()
+	}
 	if l < 1 {
 		panic(errors.New("l should > 0"))
 	}
@@ -37,5 +49,6 @@ func Uuid(l int) string {
 		panic(err)
 	}
 	s := fmt.Sprintf("%X", sl)
+	fmt.Println("len is ", l, "truth is ", len(s), "#######", s)
 	return s[:l]
 }
